c75c436f82b508485e4c6e67398b28640b8ceda3
Enrique Padron Spångberg <enrique.padron853@gmail.com>
2021-03-29 21:58:01 +0200

    funciona

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,64 @@
+let pagina = 1;
+
 document.addEventListener('DOMContentLoaded', function () {
 	iniciarApp();
 });
 
 function iniciarApp() {
 	mostrarServicios();
+
+	// Resalta el Div actual segun el tab al que se prsiona
+	//mostrarSeccion();
+
+	// Oculta o muiestra una seccion segun el tab al que se presiona
+	cambiarSeccion();
+}
+/*
+function mostrarSeccion() {
+	const seccionActual = document.querySelector(`#paso-${pagina}`);
+	seccionActual.classList.add('mostrar-seccion');
+
+    // Resalta el tab Actual
+    const tab = document.querySelector(`[data-paso="${pagina}"]`);
+    tab.classList.add('actual');
+
+}*/
+
+
+function cambiarSeccion() {
+	const enlaces = document.querySelectorAll('.tabs button');
+
+    /* Un EventListener tiene que ser agregado sobre un elemento, no puede ser agregado sobre una coleccion de elementos
+    por eso usamos el forEach*/
+	enlaces.forEach(enlace => {
+		enlace.addEventListener('click', e => {
+			e.preventDefault();
+           pagina = parseInt(e.target.dataset.paso);
+           
+            // Agrega mostrar-seccion donde dimos click
+
+			const seccion = document.querySelector(`#paso-${pagina}`);
+			seccion.classList.add('mostrar-seccion');
+
+           
+ /*   
+            // Agrega mostrar-seccion donde dimos click
+			const seccion = document.querySelector(`#paso-${pagina}`);
+			seccion.classList.add('mostrar-seccion');
+        
+			// Eliminar mostrar-seccion de la seccion anterior
+			document.querySelector('.mostrar-seccion').classList.remove('.mostrar-seccion');
+
+            // Eliminar la clase de actual en el tab anterior
+            document.querySelector('.tabs .actual').classList.remove('actual');
+
+            // Agregar la clase de actual en el nuevo tab
+            const tabActual = document.querySelector(`[data-paso="${pagina}"]`);
+            tab.classList.add('actual');
+
+        */
+		});
+	});
 }
 
 async function mostrarServicios() {
@@ -24,15 +79,45 @@ async function mostrarServicios() {
 			const nombreServicio = document.createElement('P');
 			nombreServicio.textContent = nombre;
 			nombreServicio.classList.add('nombre-servicio');
-			console.log(nombreServicio);
 
 			// Generar precioServicio
 			const precioServicio = document.createElement('P');
 			precioServicio.textContent = `$ ${precio}`;
 			precioServicio.classList.add('precio-servicio');
-			console.log(precioServicio);
+
+			// Generar div contenedor de servicio
+			const servicioDiv = document.createElement('DIV');
+			servicioDiv.classList.add('servicio');
+			servicioDiv.dataset.idServicio = id;
+
+			// Selecciona un servicio para la cita
+			servicioDiv.onclick = seleccionarServicio;
+
+			// Inyectar precio y nombre al div de servicio
+			servicioDiv.appendChild(nombreServicio);
+			servicioDiv.appendChild(precioServicio);
+
+			//Inyectarlo en el HTML
+			document.querySelector('#servicios').appendChild(servicioDiv);
 		});
 	} catch (error) {
 		console.log(error);
+	}
+}
+
+function seleccionarServicio(e) {
+	// Forzar que el elemento al cual le damos click sea el DIV
+	let elemento;
+	if (e.target.tagName === 'P') {
+		elemento = e.target.parentElement;
+	} else {
+		elemento = e.target;
+	}
+
+	// El siguiente if selecciona o des-selecciona el lemento de acuerdo a los click
+	if (elemento.classList.contains('seleccionado')) {
+		elemento.classList.remove('seleccionado');
+	} else {
+		elemento.classList.add('seleccionado');
 	}
 }
